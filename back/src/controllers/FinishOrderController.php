fa49a3d7c2ebb50eca23f90676b4329f8ddb6b18
guiottijoao <joaovitorguiotti8@gmail.com>
2026-03-27 15:40:49 -0300

    FEAT: Implement stock dicount when order is finished

--- back/src/controllers/FinishOrderController.php
+++ back/src/controllers/FinishOrderController.php
@@ -10,19 +10,28 @@ class FinishOrderController
     $this->db = $db;
   }
 
+  private function discountStock(array $orderItem)
+  {
+    $productId = $orderItem['product_code'];
+    $orderItemAmount = $orderItem['amount'];
+
+    $this->db->query("UPDATE products p SET amount = amount - '$orderItemAmount' WHERE p.code = '$productId'");
+  }
+
   public function finish()
   {
     try {
       $order_select_stmt = $this->db->query("SELECT * FROM orders o WHERE o.status = 'open'");
       $openOrder = $order_select_stmt->fetch(PDO::FETCH_ASSOC);
       if ($openOrder) {
-        error_log("Joined first if");
         $openOrderId = $openOrder['code'];
-        $order_item_select_stmt = $this->db->query("SELECT * FROM order_item ot WHERE ot.order_code = '$openOrderId'");
-        $orderItems = $order_item_select_stmt->fetch(PDO::FETCH_ASSOC);
-        
+        $order_item_select_stmt = $this->db->query("SELECT * FROM order_item oi WHERE oi.order_code = '$openOrderId'");
+        $orderItems = $order_item_select_stmt->fetchAll(PDO::FETCH_ASSOC);
+
         if ($orderItems) {
-          error_log("Joined first if");
+          foreach ($orderItems as $item) {
+            $this->discountStock($item);
+          }
           $open_order_update_stmt = $this->db->prepare("UPDATE orders SET status = 'closed'");
           $open_order_update_stmt->execute();
         }
